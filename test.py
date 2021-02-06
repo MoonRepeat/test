fbc59be356aad05e10dbd8d4b34ce34222494a3c
moonrepeat <moonrepeat@gmail.com>
2021-02-06 07:48:40 +0000

    test1

---
+++ test.py
@@ -0,0 +1,14 @@
+print("Hello World")
+from tkinter import *
+root = Tk()
+ 
+lbl = Label(root, text="이름")
+lbl.pack()
+ 
+txt = Entry(root)
+txt.pack()
+ 
+btn = Button(root, text="OK")
+btn.pack()
+ 
+root.mainloop()
